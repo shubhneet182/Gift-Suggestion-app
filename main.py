49a009a3fe5e863aaf6c74ff7088f27abdd86c34
Shubhneet <shubhneet4sandhu@gmail.com>
2021-12-24 11:08:44 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from routes.gifts import gift
+import fastapi.middleware.cors as cors
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:8000",
+    "https://rocky-garden-37686.herokuapp.com/"
+    ]
+
+app.add_middleware(
+    cors.CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET", "PUT"],
+    allow_headers=["*"],
+)
 
 # @app.get("/items/")
 # async def read_item(gender: str, age: str, occasion: str):
